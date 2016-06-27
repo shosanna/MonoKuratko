3eab8f80f88382a65cc42643ce7fb90eb6968691
Zuzana Dostalova <dostalovaz@gmail.com>
2016-06-28 00:28:56 +0200

    Boruvky

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using Nez.Tiled;
 using Microsoft.Xna.Framework.Media;
 using Nez.Textures;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MonoKuratko
 {
@@ -25,14 +26,15 @@ namespace MonoKuratko
 
     public class KuratkoGame : Core
     {
-        
+        private Kuratko _kuratko;
+        private static Map _map;
+        static Sprite<Animations> _animation;
+
         enum Animations
         {
             Default,
             Walk,
         }
-
-        static Sprite<Animations> _animation;
 
         public KuratkoGame(int width = 1280, int height = 720, bool isFullScreen = false,
             bool enableEntitySystems = true, string windowTitle = "Nez")
@@ -53,69 +55,102 @@ namespace MonoKuratko
             MediaPlayer.Play(_hudbik);
 
             // TILED MAPA 
-            var tiledEntity = defaultScene.createEntity("tiled-map");
-            var tiledMap = contentManager.Load<TiledMap>("mapa");
-            tiledEntity.addComponent(new TiledMapComponent(tiledMap));
-
+            _map = new Map(defaultScene);
+            
             // KURATKO - PLAYER
-            var kuratko = defaultScene.createEntity("kuratko");
-            kuratko.addComponent(new SimpleMover());
-
-            // ANIMACE
-            var texture = defaultScene.contentManager.Load<Texture2D>("sprites/zviratka");
-            var subtextures = Subtexture.subtexturesFromAtlas(texture, 32, 32);
-            _animation = kuratko.addComponent(new Sprite<Animations>(subtextures[8]));
-
-            _animation.addAnimation(Animations.Default, new SpriteAnimation(new List<Subtexture>()
-            {
-                subtextures[8],
-            }));
-
-            _animation.addAnimation(Animations.Walk, new SpriteAnimation(new List<Subtexture>()
-            {
-
-                subtextures[9],
-                subtextures[8],
-                subtextures[16],
-
-            }));
-
+            _kuratko = new Kuratko(defaultScene);
 
             Core.scene = defaultScene;
         }
 
-        public class SimpleMover : Component, IUpdatable
+        class Map
+        {
+            public Entity entity;
+            
+            public Map(Scene defaultScene)
+            {
+                entity = defaultScene.createEntity("tiled-map");
+                var tiledMap = contentManager.Load<TiledMap>("mapa");
+
+                // Pozadi a zdi
+                var tiledComponent = entity.addComponent(new TiledMapComponent(tiledMap, "Collision"));
+                tiledComponent.setLayersToRender("Background", "Boruvky");
+
+                // Boruvky
+                var boruvkyComponent = entity.addComponent(new TiledMapComponent(tiledMap, "Boruvky"));
+                boruvkyComponent.setLayersToRender("Boruvky");
+            }
+        }
+
+        class Kuratko
+        {
+            public Entity entity;
+            private Mover _mover;
+            private KuratkoInputHandler _inputHandler;
+            private CircleCollider _collider;
+
+            public Kuratko(Scene defaultScene)
+            {
+                entity = defaultScene.createEntity("entity", new Vector2(50, 50));
+                _inputHandler = entity.addComponent(new KuratkoInputHandler());
+                _mover = entity.addComponent(new Mover());
+
+                _collider = entity.colliders.add(new CircleCollider());                
+
+                // ANIMACE
+                var texture = defaultScene.contentManager.Load<Texture2D>("sprites/zviratka");
+                var subtextures = Subtexture.subtexturesFromAtlas(texture, 32, 32);
+                _animation = entity.addComponent(new Sprite<Animations>(subtextures[8]));
+
+                _animation.addAnimation(Animations.Default, new SpriteAnimation(new List<Subtexture>() {
+                    subtextures[8],
+                }));
+
+                _animation.addAnimation(Animations.Walk, new SpriteAnimation(new List<Subtexture>() {
+                    subtextures[9],
+                    subtextures[8],
+                    subtextures[16],
+                }));
+            }
+        }
+
+        public class KuratkoInputHandler : Component, IUpdatable
         {
             public void update()
             {
                 var animation = Animations.Default;
 
-                if (Input.isKeyDown(Keys.W)) { 
-                    entity.transform.position += new Vector2(0, -1);
+                var mover = entity.getComponent<Mover>();
+
+                CollisionResult collisionResult;
+
+                if (Input.isKeyDown(Keys.W)) {
+                    mover.move(new Vector2(0, -1), out collisionResult);
                 }
 
                 if (Input.isKeyDown(Keys.S)) {
-                    entity.transform.position += new Vector2(0, 1);
+                    mover.move(new Vector2(0, 1), out collisionResult);
                 }
 
                 if (Input.isKeyDown(Keys.A)) {
-                    entity.transform.position += new Vector2(-1, 0);
+                    mover.move(new Vector2(-1, 0), out collisionResult);
                 }
 
-                if (Input.isKeyDown(Keys.D)) { 
-                    entity.transform.position += new Vector2(1, 0);
+                if (Input.isKeyDown(Keys.D)) {
+                    mover.move(new Vector2(1, 0), out collisionResult);
                 }
 
-                if (Input.isKeyDown(Keys.W) || Input.isKeyDown(Keys.S) || Input.isKeyDown(Keys.A) || Input.isKeyDown(Keys.D)) 
-                {
+                if (Input.isKeyDown(Keys.W) || Input.isKeyDown(Keys.S) || Input.isKeyDown(Keys.A) ||
+                    Input.isKeyDown(Keys.D)) {
+
+
+                    var tile =  _map.entity.getComponents<TiledMapComponent>().Last().getTileAtWorldPosition(entity.transform.position);
+
                     animation = Animations.Walk;
                     if (!_animation.isAnimationPlaying(animation))
                         _animation.play(animation);
-                }
-                else
-                {
+                } else {
                     _animation.stop();
-  
                 }
             }
         }
